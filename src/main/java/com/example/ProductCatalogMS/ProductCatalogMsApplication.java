a4c5445c04053d7a83b1407a75a9c9fece1ebb48
Maurilio-lptech <maurilio.sperandeo@liparipeople.it>
2025-10-22 17:10:06 +0200

    first commit

---
+++ src/main/java/com/example/ProductCatalogMS/ProductCatalogMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProductCatalogMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogMsApplication.class, args);
+	}
+
+}
